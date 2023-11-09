c13e167068ddf6c368d3eb16b84235cde822e11f
Dani Kasti <danikasti7@gmail.com>
2023-11-08 19:07:06 -0500

    open cv code3

--- es.py
+++ es.py
@@ -254,7 +254,7 @@ def main():
             # Calculate the SHA-256 hash of the image data
             image_data = encoded_image.tobytes()
             image_hash = calculate_sha256_hash(image_data)
-            print("id: " ,image_data)
+            #print("id: " ,image_data)
             # Save the image as a JPEG file
             image_filename = "captured_image.jpg"
             cv2.imwrite(image_filename, image)
