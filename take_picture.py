1aa7d6a22e8d8b575a2fcafde4a0dce25237d082
John Dale <Johnkdale02@gmail.com>
2023-11-15 15:38:39 -0500

    picture taking, no button

--- take_picture.py
+++ take_picture.py
@@ -3,22 +3,9 @@
 from pynput.mouse import Listener
 import cv2
 
-# Define a global variable to store the captured image
-captured_image = None
-
 def return_image():
-    global captured_image  # Declare the global variable
 
-    # Create a listener that calls the on_click function when a mouse click event occurs
-    listener = Listener(on_click=on_click)
-    listener.start()
-
-    # Capture the image in the main thread
     captured_image = capture_image()
-
-    # Stop the listener when image capture is done
-    listener.stop()
-    listener.join()
 
     if captured_image is not None:
         print("Image not None, now save")
@@ -32,12 +19,13 @@ def return_image():
         return None
 
 
-
+'''
 def on_click(x, y, button, pressed):
     global captured_image  # Declare the global variable
     if pressed:
         print("Capture an Image...")
         captured_image = capture_image()
+'''
         
 
 def capture_image():
