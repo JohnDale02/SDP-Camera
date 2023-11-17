301d74327776845f7a6f89982f7c81b2399cbbb8
John Dale <116762794+JohnDale02@users.noreply.github.com>
2023-11-16 21:42:46 -0500

    Update tpm_commands.py

--- tpm_commands.py
+++ tpm_commands.py
@@ -6,7 +6,7 @@ tpm2_create -G rsa -u rsa.pub -r rsa.priv -C primary.ctx
 tpm2_load -C primary.ctx -u rsa.pub -r rsa.priv -n key.ctx
 tpm2_evictcontrol -C o -c key.ctx 0x81010001  # make the key persistent
 
-tpm2_sign -c key.ctx -g sha256 -o signature.bin hashfile  # used to sign hashdata file using key.ctx and save it to signature.bin
+tpm2_sign -c 0x81010001 -g sha256 -o signature.bin hashfile  # used to sign hashdata file using key.ctx and save it to signature.bin
 
 sudo tpm2_getcap handles-persistent  # used to show the persistent objects / keys
 
