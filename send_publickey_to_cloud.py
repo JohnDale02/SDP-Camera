c4ecb6b8bc72d54b1329c45bb3d80fad3a266ef3
John Dale <Johnkdale02@gmail.com>
2023-11-18 17:12:01 -0500

    f

--- send_publickey_to_cloud.py
+++ send_publickey_to_cloud.py
@@ -6,10 +6,14 @@ file_path = 'public_key.pem'
 # Read the file in binary mode
 with open(file_path, 'rb') as file:
     binary_content = file.read()
+
 print(binary_content)
+
 # Encode the binary data to a Base64 string for easy handling and display
 encoded_content = base64.b64encode(binary_content).decode('utf-8')
-byjtes = encoded_content.encode('utf-8')
+
+byjtes = base64.b64decode(encoded_content.encode('utf-8'))
+
 print(byjtes)
 decoded = base64.b64decode(encoded_content)
 
