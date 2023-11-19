9906df52ba08eb80fd1f94d03bdbead74e4dc4de
John Dale <Johnkdale02@gmail.com>
2023-11-19 18:39:12 -0500

    This it

--- create_metadata.py
+++ create_metadata.py
@@ -3,7 +3,7 @@
 def create_metadata(camera_number : str, time_data : str, location_data : str, signature_base64 : bytes):
     '''Cretes dictionary with all metadata; Strings for number, time, location, base64 (bytes) for signature'''
    
-    signature = signature.decode('utf-8')
+    signature = signature_base64.decode('utf-8')
     metadata = {}
     metadata['CameraNumber'] = camera_number
     metadata['Time'] = time_data
