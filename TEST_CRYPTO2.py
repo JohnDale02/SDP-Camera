b34d4e43e9611fad2522fa25baa9c7409e7b2ade
John Dale <Johnkdale02@gmail.com>
2023-11-19 14:14:31 -0500

    Crypot2

---
+++ TEST_CRYPTO2.py
@@ -0,0 +1,34 @@
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
+
+# Load the public key
+with open("public_key.pem", "rb") as key_file:
+    public_key = serialization.load_pem_public_key(
+        key_file.read(),
+        backend=default_backend()
+    )
+
+# Load the signature
+with open("signature.file", "rb") as signature_file:
+    signature = signature_file.read()
+
+# Load the raw data
+with open("combined.file", "rb") as data_file:
+    data = data_file.read()
+
+# Verify the signature
+try:
+    public_key.verify(
+        signature,
+        data,
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+    print("Signature is valid.")
+except Exception as e:
+    print("Signature is invalid.", e)
