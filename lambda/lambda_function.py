4e342c1b592b050227134486b78453983dfd677a
John Dale <Johnkdale02@gmail.com>
2023-11-20 11:40:38 -0500

    This is it

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -4,7 +4,11 @@ from get_public_key import get_public_key
 from verify_signature import verify_signature
 from recreate_data import recreate_data
 from upload_verified import upload_verified
+from create_combined import create_combined
 import base64
+import cv2
+import numpy as np
+
 
 def lambda_function(event, context):
     # Create an S3 client
@@ -17,41 +21,51 @@ def lambda_function(event, context):
     try:
         # Get the object from S3
         try:
+            #bucket_name = 'unverifiedimages'
+            #object_key = 'NewImage.png'
             response = s3_client.get_object(Bucket=bucket_name, Key=object_key)
+            print("got an object")
 
         except Exception as e:
             print(f"Get object error : {e}")
 
-        # Access the object's content
-        image_base64 = response['Body'].read()   # this is the base64 encoded image
-        temp_image_path = 'NewImage.png'   # recreate the png using the cv2 png object bytes recieved
+       # Access the image content
+        temp_image_path = 'TempNewImage.png'   # recreate the png using the cv2 png object bytes recieved
+        s3_client.download_file(bucket_name, object_key, temp_image_path)
         #temp_image_path = '/tmp/image.png'
+        image = cv2.imread(temp_image_path)
 
         # Access the object's metadata
         metadata = response['Metadata']
 
         try:
-            camera_number, time_data, location_data, signature = recreate_data(image_base64, metadata, temp_image_path)
+            camera_number, time_data, location_data, signature = recreate_data(metadata)
 
         except Exception as e:
             print(f"Cannot recreate time and metadata {e}")
         
         try:
-            public_key_base64 = get_public_key(camera_number)  # get the public key by using camera number string
+            public_key_base64 = get_public_key(int(camera_number))
             public_key = base64.b64decode(public_key_base64)
 
         except Exception as e:
             print(f"Public key error: {e}")
 
         try:
-            valid = verify_signature(temp_image_path, camera_number, time_data, location_data, signature, public_key)
+            combined_data = create_combined(camera_number, image, time_data, location_data)
+
+        except Exception as e:
+            print(f"Couldnt combine Data: {e}")
+        try:
+            
+            valid = verify_signature(combined_data, signature, public_key)
 
         except Exception as e:
             print(f"Error verifying or denying signature {e}")
 
         if valid == True:
             upload_verified(s3_client, camera_number, time_data, location_data, signature, temp_image_path)
-
+            print("valid signature")
         else:
             print("Signature is anything but valid")
         
@@ -63,6 +77,3 @@ def lambda_function(event, context):
         'statusCode': 200,
         'body': json.dumps('Function executed successfully!')
     }
-
-
-
