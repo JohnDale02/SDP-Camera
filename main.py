b7d7faf44be6518b4c4c52b91a8cd614ed7f45f3
John Dale <Johnkdale02@gmail.com>
2023-11-19 20:18:08 -0500

    tjis

--- main.py
+++ main.py
@@ -49,6 +49,7 @@ def main(camera_number_string):
 
 	metadata = create_metadata(camera_number_string, time, location, signature_string)   # creates a dictionary for the strings [string, string, string, byte64]
 	#print(f"Metadata: {metadata}")
+	print(metadata)
 
 #------------------ Check if we have Wi-FI -----------------------------
 
