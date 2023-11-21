7a8557a50572823075d934171e52b887b68ffe6b
Dani Kasti <danikasti7@gmail.com>
2023-11-21 16:42:18 -0500

    open cv code3

--- GPS_test.py
+++ GPS_test.py
@@ -1,54 +1,26 @@
-# from ublox_gps import UbloxGps
-# import spidev
-# import time
-
-# # Configure your SPI settings
-# spi_bus = 0  # Replace with the appropriate SPI bus number
-# spi_device = 0  # Replace with the appropriate SPI device (chip select) number
-# spi_speed = 500000  # Replace with the desired SPI speed (Hz)
-
-# # Initialize SPI
-# spi = spidev.SpiDev()
-# spi.open(spi_bus, spi_device)
-# spi.max_speed_hz = spi_speed
-
-# gps = UbloxGps(spi_device)
-
-# try:
-#     print("Listening for UBX Messages.")
-#     while True:
-#         try:
-#             coords = gps.geo_coords()
-#             print(coords.lon, coords.lat)
-#         except (ValueError, IOError) as err:
-#             print(err)
-#         time.sleep(1)
-
-# finally:
-#     spi.close()
-
+import spidev
 
 from ublox_gps import UbloxGps
-import serial
-# Can also use SPI here - import spidev
-# I2C is not supported
 
-port = serial.Serial('/dev/serial0', baudrate=38400, timeout=1)
+port = spidev.SpiDev()
 gps = UbloxGps(port)
 
 def run():
-  
-  try: 
-    print("Listenting for UBX Messages.")
-    while True:
-      try: 
-        coords = gps.geo_coords()
-        print(coords.lon, coords.lat)
-      except (ValueError, IOError) as err:
-        print(err)
-  
-  finally:
-    port.close()
+
+    try:
+        print("Listening for UBX Messages")
+        while True:
+            try:
+                geo = gps.geo_coords()
+                print("Longitude: ", geo.lon) 
+                print("Latitude: ", geo.lat)
+                print("Heading of Motion: ", geo.headMot)
+            except (ValueError, IOError) as err:
+                print(err)
+
+    finally:
+        port.close()
+
 
 if __name__ == '__main__':
-  run()
+    run()
