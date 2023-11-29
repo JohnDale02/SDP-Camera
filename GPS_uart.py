9f8609579088da71f4c8fdacaee38261d06f0f6d
John Dale <Johnkdale02@gmail.com>
2023-11-29 00:54:47 -0500

    this good

--- GPS_uart.py
+++ GPS_uart.py
@@ -1,38 +1,49 @@
 import serial
 
-def parse_gga(sentence):
-    """Parse a GGA sentence to extract latitude and longitude."""
-    fields = sentence.split(',')
+def parse_nmea_sentence(sentence):
+    """Parse GNGGA or GNRMC sentence to extract latitude and longitude."""
+    parts = sentence.split(',')
 
-    # Latitude
-    lat_value = float(fields[2])
-    lat_hemisphere = fields[3]
-    lat_degrees = int(lat_value / 100)
-    lat_minutes = lat_value - (lat_degrees * 100)
-    latitude = lat_degrees + (lat_minutes / 60)
-    if lat_hemisphere == 'S':
-        latitude *= -1
+    if parts[0] in ['$GNGGA', '$GNRMC']:
+        # Check if the data is valid
+        if parts[0] == '$GNGGA' and parts[6] != '0':
+            valid = True
+        elif parts[0] == '$GNRMC' and parts[2] == 'A':
+            valid = True
+        else:
+            valid = False
 
-    # Longitude
-    lon_value = float(fields[4])
-    lon_hemisphere = fields[5]
-    lon_degrees = int(lon_value / 100)
-    lon_minutes = lon_value - (lon_degrees * 100)
-    longitude = lon_degrees + (lon_minutes / 60)
-    if lon_hemisphere == 'W':
-        longitude *= -1
+        if valid:
+            # Parse latitude
+            lat_value = float(parts[2])
+            lat_hemisphere = parts[3]
+            lat_degrees = int(lat_value / 100)
+            lat_minutes = lat_value - (lat_degrees * 100)
+            latitude = lat_degrees + (lat_minutes / 60)
+            if lat_hemisphere == 'S':
+                latitude *= -1
 
-    return latitude, longitude
+            # Parse longitude
+            lon_value = float(parts[4])
+            lon_hemisphere = parts[5]
+            lon_degrees = int(lon_value / 100)
+            lon_minutes = lon_value - (lon_degrees * 100)
+            longitude = lon_degrees + (lon_minutes / 60)
+            if lon_hemisphere == 'W':
+                longitude *= -1
+
+            return latitude, longitude
+    return None, None
 
 # Set up the serial connection (adjust the port and baud rate according to your setup)
-ser = serial.Serial('/dev/serial0', 9600, timeout=1)
+ser = serial.Serial('/dev/ttyS0', 9600, timeout=1)
 
 try:
     while True:
-        data = ser.readline().decode('ascii', errors='ignore').strip()
-        if data.startswith('$GPGGA'):
-            lat, lon = parse_gga(data)
-            print(f"Latitude: {lat}, Longitude: {lon}")
+        sentence = ser.readline().decode('utf-8', errors='ignore').strip()
+        latitude, longitude = parse_nmea_sentence(sentence)
+        if latitude is not None and longitude is not None:
+            print(f"Latitude: {latitude}, Longitude: {longitude}")
 except KeyboardInterrupt:
     print("Program terminated!")
 finally:
