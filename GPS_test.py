256e6ee8458e8568b23f76d46010a3adae796a0f
John Dale <Johnkdale02@gmail.com>
2023-11-28 21:11:39 -0500

    New

--- GPS_test.py
+++ GPS_test.py
@@ -1,3 +1,17 @@
+import sys
+import os
+
+# Add the directory containing your module to the Python path
+module_path = os.path.expanduser('~/Qwiic_Ublox_Gps_Py/ublox_gps_module')
+sys.path.append(module_path)
+
+# Now you can import your module and class
+from ublox_gps import UbloxGps
+
+# Use the class
+gps = UbloxGps(...)
+
+
 import spidev
 
 from ublox_gps import UbloxGps
