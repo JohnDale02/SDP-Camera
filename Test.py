5e6dd2b9c0494f3521635c2bb11d0215f35c8dff
Dani Kasti <danikasti7@gmail.com>
2023-12-02 17:29:20 -0500

    open cv code3

---
+++ Test.py
@@ -0,0 +1,16 @@
+from main import main 
+import os 
+import time 
+
+camera_number_string = "1"  # camera number used to search for public key
+if not os.path.exists(os.path.join(os.getcwd(), "tmpImages")): # make a directory for tmpImages if it doesnt exist
+    os.makedirs(os.path.join(os.getcwd(), "tmpImages"))
+save_image_filepath = os.path.join(os.getcwd(), "tmpImages")
+
+start_time = time.time()
+for i in range(50): 
+  main(camera_number_string, save_image_filepath)
+
+end_time = time.time()
+Total_time = start_time - end_time
+print(Total_time)
