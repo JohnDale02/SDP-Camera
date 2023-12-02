aa3f1282668dca6240288282678a159ec3ef908e
Dani Kasti <danikasti7@gmail.com>
2023-12-02 17:33:12 -0500

    open cv code3

--- GPS_uart.py
+++ GPS_uart.py
@@ -56,36 +56,10 @@ def parse_nmea_sentence(sentence):
             return latitude, longitude, formatted_time
     return None, None, None
 
-# Set up the serial connection (adjust the port and baud rate according to your setup)
-ser = serial.Serial('/dev/ttyS0', 9600, timeout=5)
-# def read_gps_data():
-#     try:
-#         while True:
-#             sentence = ser.readline().decode('utf-8', errors='ignore').strip()
-#             latitude, longitude, time_value = parse_nmea_sentence(sentence)
-#             if latitude is not None and longitude is not None:
-#                 print(f"Latitude: {latitude}, Longitude: {longitude}, time: {time_value}")
-#             return f"Latitude: {latitude}, Longitude: {longitude}"
-#     except KeyboardInterrupt:
-#         print("Program terminated!")
-#     finally:
-#         ser.close()
 
 def read_gps_data():
-
-    # try: 
-    #     sentence = ser.readline().decode('utf-8', errors='ignore').strip()
-    #     latitude, longitude, formatted_time = parse_nmea_sentence(sentence)
-    #     if latitude is not None and longitude is not None and formatted_time is not None:
-    #         print(f"Latitude: {latitude}, Longitude: {longitude}, time: {formatted_time}")
-    #         return latitude, longitude, formatted_time
-    # except KeyboardInterrupt:
-    #     print("Program terminated!")
-    # finally:
-    #     ser.close()
-    # return "None","None","None"
-
-
+    
+    ser = serial.Serial('/dev/ttyS0', 9600, timeout=5)
     try: 
         start_time = time.time()
         while True:
