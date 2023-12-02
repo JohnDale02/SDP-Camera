650922e451e50ca64b35123f012729c88c88f95a
Dani Kasti <danikasti7@gmail.com>
2023-12-02 18:01:22 -0500

    open cv code3

--- Test.py
+++ Test.py
@@ -8,7 +8,7 @@ if not os.path.exists(os.path.join(os.getcwd(), "tmpImages")): # make a director
 save_image_filepath = os.path.join(os.getcwd(), "tmpImages")
 
 start_time = time.time()
-for i in range(40): 
+for i in range(10): 
   main(camera_number_string, save_image_filepath)
 end_time = time.time()
 Total_time = end_time - start_time
