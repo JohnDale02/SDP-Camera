a07083ff4e03e8e59d6ee54b530e0b3513528af1
Dani Kasti <danikasti7@gmail.com>
2023-12-02 20:27:17 -0500

    open cv code3

--- checkButtonPress.py
+++ checkButtonPress.py
@@ -1,9 +1,30 @@
+# import RPi.GPIO as GPIO
+# import time
+
+# GPIO.setmode(GPIO.BCM)
+
+# button_pin = 26  # BCM pin number for GPIO 26
+
+# # Use internal pull-up resistor
+# GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# try:
+#     while True:
+#         input_state = GPIO.input(button_pin)
+#         if input_state == False:
+#             print("Button Pressed")
+#             time.sleep(0.2)  # debounce delay
+
+# finally:
+#     GPIO.cleanup()
+
+
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 
-button_pin = 26  # BCM pin number for GPIO 26
+button_pin = 26  # Change this to the GPIO pin you connected the button to
 
 # Use internal pull-up resistor
 GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
