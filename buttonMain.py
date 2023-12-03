ab3da27085f1760927a8bb4c587b3b1ed1d0e000
Dani Kasti <danikasti7@gmail.com>
2023-12-02 21:09:47 -0500

    open cv code3

--- buttonMain.py
+++ buttonMain.py
@@ -3,6 +3,16 @@ import time
 import os
 from main import main
 
+
+
+camera_number_string = "1"  # camera number used to search for public key
+if not os.path.exists(os.path.join(os.getcwd(), "tmpImages")): # make a directory for tmpImages if it doesnt exist
+    os.makedirs(os.path.join(os.getcwd(), "tmpImages"))
+save_image_filepath = os.path.join(os.getcwd(), "tmpImages")
+
+
+
+
 GPIO.setmode(GPIO.BOARD)
 
 button_pin = 37  # BCM pin number for GPIO 26
@@ -12,7 +22,7 @@ GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def button_callback(channel):
     print("Button Pressed")
-    main()  # Call the main function when the button is pressed
+    main(camera_number_string, save_image_filepath)  # Call the main function when the button is pressed
 
 # Add an interrupt event for the button press
 GPIO.add_event_detect(button_pin, GPIO.FALLING, callback=button_callback, bouncetime=200)
