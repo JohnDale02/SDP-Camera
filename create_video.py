3590acc9a856eeb69e2e174489ef9be7176b8281
John Dale <johnkdale02@gmail.com>
2024-02-06 20:36:07 -0500

    Try

--- create_video.py
+++ create_video.py
@@ -27,14 +27,15 @@ def capture_video(video_filename):
     command = [
         'ffmpeg',
         '-f', 'v4l2',
-        '-video_size', '1920x1080',
+        '-video_size', '1280x720',
         '-i', '/dev/video0',
         '-c:v', 'libvpx-vp9',  # Use the VP9 codec
-        '-lossless', 1,      # Enable lossless encoding
-        '-pix_fmt', 'yuv420p', # Use a pixel format compatible with lossless encoding
+        '-lossless', '1',      # Enable lossless encoding
+        '-pix_fmt', 'yuva420p', # Use a pixel format compatible with lossless encoding
         '-b:v', '2M',
         '-bufsize', '2M',
         '-t', '10',
+        '-f', 'webm', 
         video_filename
     ]
 
