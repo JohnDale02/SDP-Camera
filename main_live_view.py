b60c80204f33bc487e8ede0314f8f5d1500647f8
John Dale <johnkdale02@gmail.com>
2024-02-10 12:29:17 -0500

    Testing

--- main_live_view.py
+++ main_live_view.py
@@ -35,7 +35,7 @@ def record_thread():
     global image_mode
     global is_recording
 
-    mode_button, record_button = setup_gpio()
+    #mode_button, record_button = setup_gpio()
     #changeModeThread = threading.Thread(target=toggle_image_mode, args=(mode_button,), daemon=True)
     #changeRecordingThread = threading.Thread(target=toggle_recording, args=(record_button,), daemon=True)
     handleCaptureThread = threading.Thread(target=handle_capture, daemon=True)
