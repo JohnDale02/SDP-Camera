db661b85691a123d2cd6b7386f169e92de17c821
John Dale <johnkdale02@gmail.com>
2024-02-11 23:02:47 -0500

    Diff

--- main_live_view.py
+++ main_live_view.py
@@ -287,8 +287,8 @@ def capture_image():
     camera.set(cv2.CAP_PROP_FPS, 30.0)
     camera.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter.fourcc('m','j','p','g'))
     camera.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter.fourcc('M','J','P','G'))
-    camera.set(cv2.CAP_PROP_FRAME_WIDTH, 3840)
-    camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 2160)
+    camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+    camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
     Clock.schedule_once(lambda dt: gui_instance.start_countdown(duration=5), 0)
 
