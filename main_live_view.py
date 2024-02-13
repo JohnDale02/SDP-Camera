894425284e32d95a2ecf8150781654953d90870a
John Dale <johnkdale02@gmail.com>
2024-02-12 23:57:19 -0500

    Diff

--- main_live_view.py
+++ main_live_view.py
@@ -39,8 +39,8 @@ recording_indicator = False
 gps_lock = Lock()
 
 camera_number_string = "1"
-save_video_filepath = os.path.join(os.getcwd(), "SDP-Camera/tmpVideos")
-save_image_filepath = os.path.join(os.getcwd(), "SDP-Camera/tmpImages")
+save_video_filepath = os.path.join(os.getcwd(), "tmpVideos")
+save_image_filepath = os.path.join(os.getcwd(), "tmpImages")
 object_count = None
 gui_instance = None
 
