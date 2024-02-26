68d0c8941df214c0d220001570b38a9b202d43e6
John Dale <johnkdale02@gmail.com>
2024-02-25 20:45:58 -0500

    white indicators; black background

--- main_live_view.py
+++ main_live_view.py
@@ -236,7 +236,7 @@ class PhotoLockGUI(FloatLayout):
         self.img1.size = (video_width, video_height)
         self.img1.pos = ((window_width - video_width) / 2, 0)
 
-        self.indicators_bg_rect.pos=(0,0)
+        self.indicators_bg_rect.pos=(100,100)
 
         
     def update_rect(self, instance, value):
