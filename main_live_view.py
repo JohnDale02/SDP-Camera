554102f881130209ad18f797335d9da72f1cb9f3
John Dale <Johnkdale02@gmail.com>
2024-03-06 17:41:01 -0500

    money

--- main_live_view.py
+++ main_live_view.py
@@ -78,7 +78,7 @@ def setup_gpio():
 
     # Setup event detection
     GPIO.add_event_detect(mode_button, GPIO.FALLING, callback=toggle_image_mode, bouncetime=1000)
-    GPIO.add_event_detect(record_button, GPIO.FALLING, callback=toggle_recording, bouncetime=1000)
+    GPIO.add_event_detect(record_button, GPIO.FALLING, callback=toggle_recording, bouncetime=3000)
     
 # --------------------------------------------------------------------
     
