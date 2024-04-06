edc13914a5f1abdf6083ecc1611acaa3e5f76b36
John Dale <johnkdale02@gmail.com>
2024-04-06 01:58:09 -0400

    trying frame animation

--- main_live_view.py
+++ main_live_view.py
@@ -98,15 +98,11 @@ def setup_gpio():
 def fingerprint_monitor():
     global media_taken, fingerprint
     if media_taken >= 2 or fingerprint == None:
-        GPIO.remove_event_detect(mode_button)
-        GPIO.remove_event_detect(record_button)
         media_taken = 0
         fingerprint = None
 
         time.sleep(10)
         fingerprint = "John Dale"
-        GPIO.add_event_detect(mode_button, GPIO.FALLING, callback=toggle_image_mode, bouncetime=1000)
-        GPIO.add_event_detect(record_button, GPIO.FALLING, callback=toggle_recording, bouncetime=3000)
     '''
     while fingerprint == None:
         result = fingerprint_reader.search()   # find matching fingerprint 
@@ -391,6 +387,9 @@ def toggle_image_mode(channel):
     global recording_indicator
     global camera
 
+    if not fingerprint:
+        return 
+
     with record_lock:
         if not recording_indicator:
             image_mode = not image_mode
@@ -421,8 +420,12 @@ def toggle_recording(channel):
     global camera
     global gui_instance
     global media_taken
+    global fingerprint
 
     if recording_indicator and not mid_video:
+        return 
+    
+    if not fingerprint:
         return 
     
     with record_lock:
