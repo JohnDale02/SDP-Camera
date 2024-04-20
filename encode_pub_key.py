db5aaa78ccb41a9b22cef9032c04faf4635105ab
John Dale <johnkdale02@gmail.com>
2024-04-20 13:33:31 -0400

    fingerptin

--- encode_pub_key.py
+++ encode_pub_key.py
@@ -2,7 +2,7 @@ import base64
 
 def encode_key_to_base64(file_path):
     # Open and read the public key file
-    with open(file_path, 'r') as file:
+    with open(file_path, 'rb') as file:
         public_key = file.read()
     
     # The public key file typically starts with '-----BEGIN PUBLIC KEY-----'
@@ -11,7 +11,7 @@ def encode_key_to_base64(file_path):
     start_marker = "-----BEGIN PUBLIC KEY-----"
     end_marker = "-----END PUBLIC KEY-----"
     
-    base64_encoded_public_key = base64.b64encode(public_key.encode()).decode()
+    base64_encoded_public_key = base64.b64encode(public_key.encode())
     
     print("Encoded Public Key:", base64_encoded_public_key)
 
