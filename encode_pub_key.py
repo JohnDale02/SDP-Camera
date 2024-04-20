6dddd8b767e9bf46ca1ba40b7f0f8701f7a7ec48
John Dale <johnkdale02@gmail.com>
2024-04-20 13:27:07 -0400

    fingerptin

--- encode_pub_key.py
+++ encode_pub_key.py
@@ -14,13 +14,7 @@ def encode_key_to_base64(file_path):
     # Strip headers/footers and replace newlines
     encoded_key = public_key.replace(start_marker, '').replace(end_marker, '').strip()
     
-    # Base64 decode, then re-encode to ensure it's in clean base64 format
-    # This step depends on the formatting of your key file. If it's already base64-encoded,
-    # this decode-encode step can be adjusted or skipped as necessary.
-    key_bytes = base64.b64decode(encoded_key.encode())
-    clean_base64_encoded_key = base64.b64encode(key_bytes).decode('utf-8')
-    
-    return clean_base64_encoded_key
+    print("Encoded Public Key:", encoded_key)
 
 # Specify the path to your public key file
 file_path = '/home/sdp/rsa.pub'
