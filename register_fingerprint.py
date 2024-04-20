f526aae1b81292538ff48566930708ae795a0c45
John Dale <johnkdale02@gmail.com>
2024-04-19 21:27:05 -0400

    fingerptin

--- register_fingerprint.py
+++ register_fingerprint.py
@@ -1,14 +1,49 @@
+from pyfingerprint.pyfingerprint import PyFingerprint
+import time
 
-from r305 import R305
+try:
+    ## Change here: Update the port to '/dev/ttyAMA1' or the correct one for UART4
+    f = PyFingerprint('/dev/ttyAMA4', 57600, 0xFFFFFFFF, 0x00000000)
 
+    if (f.verifyPassword() == False):
+        raise ValueError('The given fingerprint sensor password is wrong!')
 
-device = "/ttyAMA4"
-baudrate = 57600 # the default baudrate for this module is 57600
+except Exception as e:
+    print('The fingerprint sensor could not be initialized!')
+    print('Exception message: ' + str(e))
+    exit(1)
 
-dev = R305(device, baudrate)
+try:
+    print('Waiting for finger...')
+    while (f.readImage() == False):
+        pass
 
-def callback(data):
-    x = raw_input(data)
+    f.convertImage(0x01)
+    result = f.searchTemplate()
+    positionNumber = result[0]
 
-result = dev.StoreFingerPrint(IgnoreChecksum=True, callback=callback)
-print(result)
+    if (positionNumber >= 0):
+        print('Template already exists at position #' + str(positionNumber))
+        exit(0)
+
+    print('Remove finger...')
+    time.sleep(2)
+    print('Waiting for same finger again...')
+
+    while (f.readImage() == False):
+        pass
+
+    f.convertImage(0x02)
+
+    if (f.compareCharacteristics() == 0):
+        raise Exception('Fingers do not match')
+
+    f.createTemplate()
+    positionNumber = f.storeTemplate()
+    print('Finger enrolled successfully!')
+    print('New template position #' + str(positionNumber))
+
+except Exception as e:
+    print('Operation failed!')
+    print('Exception message: ' + str(e))
+    exit(1)
