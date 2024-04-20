d69f0debe040805d9cd3aae5d7be177ece60d67a
John Dale <johnkdale02@gmail.com>
2024-04-20 15:02:27 -0400

    fingerptin

--- get_fingerprint.py
+++ get_fingerprint.py
@@ -1,36 +1,42 @@
 from pyfingerprint.pyfingerprint import PyFingerprint
 
-try:
-    ## Change here: Update the port to '/dev/ttyAMA4'
-    f = PyFingerprint('/dev/ttyAMA4', 57600, 0xFFFFFFFF, 0x00000000)
+def get_fingerprint():
+    try:
+        ## Change here: Update the port to '/dev/ttyAMA4'
+        f = PyFingerprint('/dev/ttyAMA5', 57600, 0xFFFFFFFF, 0x00000000)
 
-    if (f.verifyPassword() == False):
-        raise ValueError('The given fingerprint sensor password is wrong!')
+        if (f.verifyPassword() == False):
+            raise ValueError('The given fingerprint sensor password is wrong!')
 
-except Exception as e:
-    print('The fingerprint sensor could not be initialized!')
-    print('Exception message: ' + str(e))
-    exit(1)
+    except Exception as e:
+        print('The fingerprint sensor could not be initialized!')
+        print('Exception message: ' + str(e))
+        exit(1)
 
-try:
-    print('Waiting for finger...')
+    try:
+        print('Waiting for finger...')
 
-    while (f.readImage() == False):
-        pass
+        while (f.readImage() == False):
+            pass
 
-    f.convertImage(0x01)
-    result = f.searchTemplate()
+        f.convertImage(0x01)
+        result = f.searchTemplate()
 
-    positionNumber = result[0]
-    accuracyScore = result[1]
+        positionNumber = result[0]
+        accuracyScore = result[1]
 
-    if (positionNumber == -1):
-        print('No match found!')
-    else:
-        print('Found template at position #' + str(positionNumber))
-        print('The accuracy score is: ' + str(accuracyScore))
+        if (positionNumber == -1):
+            print('No match found!')
+            return -1
+        else:
+            print('Found template at position #' + str(positionNumber))
+            print('The accuracy score is: ' + str(accuracyScore))
+            return positionNumber
 
-except Exception as e:
-    print('Operation failed!')
-    print('Exception message: ' + str(e))
-    exit(1)
+    except Exception as e:
+        print('Operation failed!')
+        print('Exception message: ' + str(e))
+        exit(1)
+
+
+print(get_fingerprint())
